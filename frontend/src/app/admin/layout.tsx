871263314226e0158ffd6130d039866076ca0d2e
Tanishq <Tbabbar65@gmail.com>
2026-03-13 11:34:20 +0530

    fix nextjs admin layout

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,13 +1,7 @@
-import React from "react";
-
-export default function AdminLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <section>
-      {children}
-    </section>
-  );
+  return <>{children}</>;
 }
